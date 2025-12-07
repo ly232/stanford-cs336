f8e7f3173a222f8b60cb0da7f505666fa682afa1
Lin Yang <yang7.cooper@gmail.com>
2025-12-07 09:37:52 -0800

    Implement scaled dot product attention.

--- cs336_basics/model/utils.py
+++ cs336_basics/model/utils.py
@@ -1,14 +1,47 @@
 """Custom implementations of various helpers"""
 
 from einops import einsum
+from jaxtyping import Bool, Float
+from torch import Tensor
 from torch.nn import functional as F
 
+import math
 import torch
 import torch.nn as nn
 
-def softmax(x: torch.Tensor, dim: int) -> torch.Tensor:
-    """Compute softmax over the specified dimension."""
+def softmax(x: Tensor, dim: int) -> Tensor:
+    """Compute softmax over the specified dimension.
+    
+    Returns a tensor of same shape.
+    """
     x_max = x.max(dim=dim, keepdim=True).values
     exp_x = torch.exp(x - x_max)
-    return exp_x / exp_x.sum(dim=dim, keepdim=True)
+    result = exp_x / exp_x.sum(dim=dim, keepdim=True)
+    assert x.shape == result.shape
+    return result
 
+def scaled_dot_product_attention(
+    Q: Float[Tensor, " ... queries d_k"],  # (..., n, dk)
+    K: Float[Tensor, " ... keys d_k"],  # (..., m, dk)
+    V: Float[Tensor, " ... values d_v"],  # (..., m, dv)
+    mask: Bool[Tensor, " ... queries keys"] | None = None,
+) -> Float[Tensor, "... queries d_v"]:
+    """
+    Given key (K), query (Q), and value (V) tensors, return
+    the output of your scaled dot product attention implementation.
+
+    Args:
+        Q (Float[Tensor, " ... queries d_k"]): Query tensor
+        K (Float[Tensor, " ... keys d_k"]): Key tensor
+        V (Float[Tensor, " ... values d_v"]): Values tensor
+        mask (Bool[Tensor, " ... queries keys"] | None): Mask tensor
+    Returns:
+        Float[Tensor, " ... queries d_v"]: Output of SDPA
+    """
+    d_k = Q.shape[-1]
+    softmax_arg = einsum(Q, K, "... n d_k, ... m d_k -> ... n m")
+    softmax_arg /= math.sqrt(d_k)
+    if mask is not None:
+        softmax_arg = softmax_arg.masked_fill(~mask, float('-inf'))
+    attn = softmax(softmax_arg, dim=-1)  # (..., n, m)
+    return einsum(attn, V, "... n m, ... m d_v -> ... n d_v")
--- tests/adapters.py
+++ tests/adapters.py
@@ -17,7 +17,7 @@ from cs336_basics.model.embedding import Embedding
 from cs336_basics.model.rms_layer_norm import RmsLayerNorm
 from cs336_basics.model.positionwise_feedforward import PositionwiseFeedforward
 from cs336_basics.model.rotary_positional_embeddin import RotaryPositionalEmbedding
-from cs336_basics.model.utils import softmax
+from cs336_basics.model.utils import softmax, scaled_dot_product_attention
 
 def run_linear(
     d_in: int,
@@ -123,7 +123,7 @@ def run_scaled_dot_product_attention(
     Returns:
         Float[Tensor, " ... queries d_v"]: Output of SDPA
     """
-    raise NotImplementedError
+    return scaled_dot_product_attention(Q, K, V, mask)
 
 
 def run_multihead_self_attention(
