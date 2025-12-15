d778ae3e9e94af063db42bddcee93d471e396ee9
ly232 <yang7.cooper@gmail.com>
2025-12-14 22:04:11 -0800

    Implement cosine annealing learning rate scheduling.

--- cs336_basics/training/utils.py
+++ cs336_basics/training/utils.py
@@ -1,6 +1,7 @@
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
+import math
 import torch
 
 def cross_entropy(
@@ -21,3 +22,42 @@ def cross_entropy(
     target_logits = inputs.gather(dim=1, index=targets.unsqueeze(1))
     loss =  lse - target_logits
     return loss.mean(dim=0)
+
+def get_lr_cosine_schedule(
+    it: int,
+    max_learning_rate: float,
+    min_learning_rate: float,
+    warmup_iters: int,
+    cosine_cycle_iters: int,
+):
+    """
+    Given the parameters of a cosine learning rate decay schedule (with linear
+    warmup) and an iteration number, return the learning rate at the given
+    iteration under the specified schedule.
+
+    Args:
+        it (int): Iteration number to get learning rate for.
+        max_learning_rate (float): alpha_max, the maximum learning rate for
+            cosine learning rate schedule (with warmup).
+        min_learning_rate (float): alpha_min, the minimum / final learning rate for
+            the cosine learning rate schedule (with warmup).
+        warmup_iters (int): T_w, the number of iterations to linearly warm-up
+            the learning rate.
+        cosine_cycle_iters (int): T_c, the number of cosine annealing iterations.
+
+    Returns:
+        Learning rate at the given iteration under the specified schedule.
+    """
+    if it < warmup_iters:
+        # warmup
+        lr = max_learning_rate * (it / warmup_iters)
+    elif warmup_iters <= it <= cosine_cycle_iters:
+        # cosine annealing
+        lr = min_learning_rate \
+            + 0.5 * (1 + math.cos(
+                math.pi * (it - warmup_iters) / (cosine_cycle_iters - warmup_iters))) \
+            * (max_learning_rate - min_learning_rate)
+    else:
+        # post annealing
+        lr = min_learning_rate
+    return lr
